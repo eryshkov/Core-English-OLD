593cf022ec4305e8d8bdd5691ca86ce5cf19f7cb
eryshkov <wendor265>
2018-09-24 00:49:58 +0300

    fix constraints

--- Core English/ViewController.swift
+++ Core English/ViewController.swift
@@ -9,15 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var fieldView: UIView!
+    
+    var verticalConstraint: NSLayoutConstraint?
+    var horizontalConstraint: NSLayoutConstraint?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
 //        FillVocabulary.context.fill()
-        
+        createConstraints()
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setConstraints()
+    }
+    
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        setConstraints()
+    }
+    
+    func createConstraints() {
+        horizontalConstraint = NSLayoutConstraint(item: fieldView, attribute: .height, relatedBy: .equal, toItem: view, attribute: .height, multiplier: 0.9, constant: 0)
+//        horizontalConstraint.identifier = "forHorizontal"
+        
+        verticalConstraint = NSLayoutConstraint(item: fieldView, attribute: .width, relatedBy: .equal, toItem: view, attribute: .width, multiplier: 0.9, constant: 0)
+//        verticalConstraint.identifier = "forVertical"
+    }
+    
+    func setConstraints() {
+        if UIApplication.shared.statusBarOrientation.isLandscape {
+            verticalConstraint?.isActive = false
+            horizontalConstraint?.isActive = true
+            print("Horizontal")
+        } else {
+            horizontalConstraint?.isActive = false
+            verticalConstraint?.isActive = true
+            print("Vertical")
+        }
+    }
 
 }
 
